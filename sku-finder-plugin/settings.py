2e8eb9fd3960509986384d00207399ef1b96df7b
Anuj Saini <anujhsrsaini@gmail.com>
2023-02-09 11:34:02 +0530

    To push to AWS First time

--- sku-finder-plugin/settings.py
+++ sku-finder-plugin/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import datetime
 from datetime import timedelta
-
+import os
 import environ
 env = environ.Env(
     DEBUG=(int, 0)
@@ -175,6 +175,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
